68f708cb8160072cb21b23e431e7a9018739b245
suresh sharma <suresh.sharma510@gmail.com>
2023-08-05 15:50:35 +0530

    Project created

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 import express from 'express';
-import {Worker} from "node:worker_threads"
+import {Worker, workerData} from "node:worker_threads"
 
 const app = express();
 const port = 3000;
-const worker =new Worker("./worker.js")
+
+// Worker thread initialization
+const worker =new Worker("./worker.js",{ workerData: {
+  wData : "Hello from worker.",
+}})
 // const worker2 =new Worker("./worker2.js")
 
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
-    res.send('Light task completed');
+  console.log("From index.js file");
+  res.send('Light task completed test');
 });
+
 app.get('/heavy', (req, res) => {
-    
+  worker.postMessage("message")
 
-    worker.postMessage("heavyTask-30")
-    // worker.close()
+  setTimeout(() => {
+    worker.close()
+  }, 6000)
  
-   worker.on("heavyTask-30",()=>{
-       res.send(`Heavy task Completed-${123}`)
-
-   })
-  });
+  // Worker event listener
+  worker.postMessage("close")
+  worker.on("message",()=>{
+    res.send(`Heavy task Completed-${123}`)
+  })
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- worker.js
+++ worker.js
@@ -1,14 +1,11 @@
-import {parentPort} from "node:worker_threads"
+import {parentPort, workerData} from "worker_threads"
 
-parentPort.on("heavyTask-30",(data)=>{
+parentPort.on("message", () => {
     const x="heavyTask-30".split("-")[1];
     let xNumber = Number(x);
-    console.log(data)
-    for(let i=0;i<300;i++){
-       
-        xNumber++;
-    }
-    parentPort.postMessage("xNumber")
+    console.log(xNumber, "from worker",workerData.wData);
+
+    parentPort.postMessage("message")
 })
 
 parentPort.on("close", () => {
