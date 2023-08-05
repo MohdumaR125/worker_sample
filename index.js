33d8604aa7da03cb01b457e602f7f0b8f6c1c104
MOHD UMAR <umar231294@gmail.com>
2023-08-05 16:49:16 +0530

    Add files via upload
    added worker

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-import express from 'express';
-import {Worker} from "node:worker_threads"
+import {tWorker} from  "node:worker-thread"
 
-const app = express();
-const port = 3000;
-const worker =new Worker("./worker.js")
-const worker2 =new Worker("./worker2.js")
+const worker = new Worker("worker.js")
 
+const str = "hello world"
 
-// Define a route for the root URL
-app.get('/', (req, res) => {
-    res.send('light task');
-});
-app.get('/heavy', (req, res) => {
-    
-
-    worker.postMessage("heavy")
-    worker.close()
- 
-   worker.on("message",(x)=>{
-       res.send("heavy task")
-
-   })
-  });
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+worker.on("message",(data)=>{
+if(data==="done"){
+worker.terminate();
+}
+})
+worker.postMessage(str)
